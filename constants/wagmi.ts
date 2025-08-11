1ba00496bc3bc20174609536d1aadcf26afdf6a4
Hasan Mukhlis <hsnmkls@proton.me>
2025-08-11 11:08:35 +0800

    Fixing passkey

--- constants/wagmi.ts
+++ constants/wagmi.ts
@@ -2,9 +2,8 @@ import { Mode } from "porto";
 import { porto } from "porto/wagmi";
 import * as passkeys from "react-native-passkeys";
 import { riseTestnet, riseTestnetConfig } from "rise-wallet";
+import { type ByteArray, bytesToHex } from "viem";
 import { createConfig, http } from "wagmi";
-
-const decoder = new TextDecoder();
 
 export const wagmiConfig = createConfig({
   chains: [riseTestnet],
@@ -16,23 +15,34 @@ export const wagmiConfig = createConfig({
     porto({
       ...riseTestnetConfig,
       mode: Mode.rpcServer({
-        keystoreHost: "https://rise-testnet-vercel.app",
+        keystoreHost: "rise-wallet-testnet.vercel.app",
         webAuthn: {
           createFn: async (opts) => {
             if (!opts?.publicKey) return null;
-            return await passkeys.create({
+            const optsNew = {
               ...opts.publicKey,
-              challenge: decoder.decode(opts.publicKey.challenge),
+              challenge: btoa(
+                bytesToHex(opts.publicKey.challenge as ByteArray),
+              ),
               user: {
                 ...opts.publicKey.user,
-                id: decoder.decode(opts.publicKey.user.id),
+                id: btoa(bytesToHex(opts.publicKey.user.id as ByteArray)),
               },
               signal: opts.signal,
-            });
+            } as const;
+            console.log(JSON.stringify(optsNew, null, 2));
+            const res = await passkeys.create(optsNew);
+            console.log(res);
+            return res;
           },
           getFn: async (opts) => {
             if (!opts?.publicKey) return null;
-            return await passkeys.get(opts.publicKey);
+            return await passkeys.get({
+              ...opts.publicKey,
+              challenge: btoa(
+                bytesToHex(opts.publicKey.challenge as ByteArray),
+              ),
+            });
           },
         },
       }),
